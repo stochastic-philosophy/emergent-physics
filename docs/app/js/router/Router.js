4a7ec88725dabcde74bee9882713d695a68b8ed5
stochastic-philosophy <stochasticphilosophy@gmail.com>
2025-07-26 00:49:49 +0300

    Create Router.js

---
+++ docs/app/js/router/Router.js
@@ -0,0 +1,50 @@
+// app/js/router/Router.js
+export class Router {
+    constructor() {
+        this.routes = {};
+        this.currentRoute = '';
+    }
+    
+    init(routes) {
+        this.routes = routes;
+        
+        // Listen to hash changes
+        window.addEventListener('hashchange', this.handleRouteChange.bind(this));
+        
+        // Handle initial route
+        this.handleRouteChange();
+    }
+    
+    handleRouteChange() {
+        const hash = window.location.hash.substring(1);
+        const route = hash || '';
+        
+        console.log(`🧭 Route change: ${route}`);
+        
+        if (this.routes[route]) {
+            this.currentRoute = route;
+            this.routes[route]();
+        } else {
+            // Try to find partial match
+            const partialMatch = Object.keys(this.routes).find(r => 
+                route.startsWith(r) && this.routes[r]
+            );
+            
+            if (partialMatch) {
+                this.currentRoute = route;
+                this.routes[partialMatch]();
+            } else {
+                console.warn(`Route not found: ${route}`);
+                this.navigate('home');
+            }
+        }
+    }
+    
+    navigate(route) {
+        window.location.hash = route;
+    }
+    
+    getCurrentRoute() {
+        return this.currentRoute;
+    }
+}
